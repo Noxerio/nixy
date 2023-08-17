461981fc96534c6fe7dc7a9eff0835407384c22c
Noxerio <woinyc@seznam.cz>
2023-08-17 11:44:55 +0200

    tailwind fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,12 @@
-module.exports = {
-  mode: "jit",
-  purge: [
-    "./assets/**/*.{css}",
-    "./components/*.{vue,js}",
-    "./components/**/*.{vue,js}",
-    "./pages/*.vue",
-    "./pages/**/*.vue",
-    "./plugins/**/*.{js,ts}",
-    "./*.{vue,js,ts}",
-    "./nuxt.config.{js,ts}",
-  ],
-  theme: {
-    extend: {},
-  },
-  variants: {
-    extend: {},
-  },
-  plugins: [],
+export const content = [
+  "./components/**/*.{js,vue,ts}",
+  "./layouts/**/*.vue",
+  "./pages/**/*.vue",
+  "./plugins/**/*.{js,ts}",
+  "./nuxt.config.{js,ts}",
+];
+export const theme = {
+  extend: {},
 };
+export const plugins = [
+];
